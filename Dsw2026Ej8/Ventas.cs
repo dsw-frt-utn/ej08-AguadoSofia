ba7242e0d21603b6f52d2bb93708359cd7ab40b3
Sofia <anasofiaaguado0@gmail.com>
2026-05-03 15:49:48 -0300

    roblema 6: Métodos de extensión finalizado

---
+++ Dsw2026Ej8/Ventas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal Amount { get; set; }
+        public virtual decimal CalculateTotal() => Amount;
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal() => Amount;
+    }
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal() => Amount * 0.9m;
+    }
+
+}
