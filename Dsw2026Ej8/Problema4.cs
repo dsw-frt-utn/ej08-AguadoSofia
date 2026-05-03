10acddddadfe86e38e3a111884b9e5cd96d4dc94
Sofia <anasofiaaguado0@gmail.com>
2026-05-03 15:40:33 -0300

    Problema 4: Cálculo de promedios con nulables

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            var notasRecibidas = new List<int?> { nota1, nota2, nota3 };
+
+            var notasValidas = notasRecibidas
+                .Where(n => n.HasValue && n.Value >= 0 && n.Value <= 10)
+                .Select(n => (double)n!.Value)
+                .ToList();
+        
+            if (notasValidas.Count == 0)
+            {
+                return 0;
+            }
+
+            return notasValidas.Average();
+        }
+    }
+}
